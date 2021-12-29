- fixed a bug with the allocation parameter gradient allocation, which was transposing the assignments of the new gradient unexpectedly - updated CartPole example

a2c_team_tf/lib/lib_mult_env.py
@@ -43,8 +43,8 @@ class Agent:
         data = {"state": state_new, "reward": step_reward, "done": done}
         self.dfas[agent].next(data)
         # print("state", state_new)
-        #if agent == 1:
-        #    print(f"agent ({agent}) ", self.dfas[agent].progress)
+        # if agent == 1:
+        #     print(f"agent ({agent}) ", self.dfas[agent].progress)
 
         # agent-task rewards
         task_rewards = self.dfas[agent].rewards(self.one_off_reward)
@@ -127,14 +127,14 @@ class Agent:
     def df(self, x: tf.Tensor) -> tf.Tensor:
         """derivative of mean squared error"""
         if tf.less_equal(x, self.c):
-            return 2 * (x - self.c)
+            return 2 * (self.c - x)
         else:
             return tf.convert_to_tensor(0.0)
 
     def dh(self, x: tf.Tensor) -> tf.Tensor:
         # print(f"x: {x}, e: {e}")
         if tf.less_equal(x, self.e):
-            return 2 * (x - self.e)
+            return 2 * (self.e - x)
         else:
             return tf.convert_to_tensor(0.0)
 
@@ -190,10 +190,10 @@ class Agent:
         H = self.compute_H(ini_value, ini_values_i, agent, mu)
         advantage = tf.matmul(returns - values, H)
         action_log_probs = tf.math.log(action_probs)
-        actor_loss = tf.math.reduce_sum(action_log_probs * advantage)
+        actor_loss = -tf.math.reduce_sum(action_log_probs * advantage)
 
         critic_loss = huber_loss(values, returns)
-        return actor_loss + critic_loss
+        return actor_loss - critic_loss
 
     def run_episode(
             self,

a2c_team_tf/tests/cartpole_tests.py
@@ -25,16 +25,22 @@ class MoveToPos(DFAStates, ABC):
         self.fail = "F"
 
 def go_left_to_pos(data, _):
-    if data['state'][0] < - 0.1 / 2:
+    if -2.0 < data['state'][0] <= -1.0: #and -45.0 < math.degrees(data['state'][2]) < 45.0:
         return "P"
+    elif data['state'][0] < -2.0 or data['state'][0] > 2.0:
+        return "F"
+    #elif not -45.0 < math.degrees(data['state'][2]) < 45.0:
+    #    return "F"
     else:
         return "I"
 
-def get_reached(data, _):
-    if data['state'][0] > 0.3 and not data['done']:
+def go_right_to_pos(data, _):
+    if 1.0 < data['state'][0] < 2.0: #and -45. < math.degrees(data['state'][2]) < 45.:
         return "P"
-    elif data['done']:
+    elif data['state'][0] < -2.0 or data['state'][0] > 2.0:
         return "F"
+    #elif not -45.0 < math.degrees(data['state'][2]) < 45.0:
+    #    return "F"
     else:
         return "I"
 
@@ -47,7 +53,7 @@ def failed(data, _):
 def make_move_to_pos_dfa():
     dfa = DFA(start_state="I", acc=["P"], rej=["F"])
     dfa.states = MoveToPos()
-    dfa.add_state(dfa.states.init, get_reached)
+    dfa.add_state(dfa.states.init, go_right_to_pos)
     dfa.add_state(dfa.states.position, finished_move)
     dfa.add_state(dfa.states.fail, failed)
     return dfa
@@ -64,7 +70,7 @@ def make_move_left_to_pos():
 # Create the environment
 envs = []
 for i in range(2):
-    envs.append(make_env('CartPole-v0', 0, seed, False))
+    envs.append(make_env('CartPole-default-v0', 0, seed + 1000 * i, False))
 
 # Set seed for experiment reproducibility
 
@@ -76,9 +82,7 @@ eps = np.finfo(np.float32).eps.item()
 
 print(envs[0].observation_space)
 
-step_rew0 = 15  # step reward threshold
-
-one_off_reward = 20.0  # one-off reward
+one_off_reward = 100.0  # one-off reward
 task_prob0 = 0.8  # the probability threhold of archieving the above task
 
 right = make_move_to_pos_dfa()
@@ -94,13 +98,13 @@ num_hidden_units = 128
 models = [ActorCritic(num_actions, num_hidden_units, num_tasks, name="AC{}".format(i)) for i in range(num_agents)]
 ## Some auxiliary functions for defining the "compute_loss" function.
 # mu = 1.0 / num_agents  # fixed even probability of allocating each task to each agent
-lam = 1.0
+lam = 1.0 / 10.0
 chi = 1.0
-c = 195
+c = -80.0
 e = task_prob0 * one_off_reward  # task reward threshold
 
 min_episodes_criterion = 100
-max_episodes = 200  # 10000
+max_episodes = 300  # 10000
 max_steps_per_episode = 200  # 1000
 
 # Cartpole-v0 is considered solved if average reward is >= 195 over 100
@@ -110,7 +114,7 @@ running_reward = 0
 ## No discount
 gamma = 1.00
 alpha1 = 0.001
-alpha2 = 0.001
+alpha2 = 0.005
 
 agent = Agent(envs=envs, dfas=dfas, e=e, c=c, chi=chi, lam=lam, gamma=gamma,
               one_off_reward=one_off_reward, num_tasks=num_tasks, num_agents=num_agents)
@@ -138,34 +142,43 @@ action_probs, values, rewards, mask = agent.run_episode(initial_state, agent_idx
 # # _, r_ = tf.dynamic_partition(rewards, mask, 2)
 # # print("r shape: ", r_.shape)
 # kappa is the allocation parameter tensor
-kappa = tf.Variable(np.full(num_agents * num_tasks, 1.0 / num_agents), dtype=tf.float32)
+kappa = tf.Variable(np.full([num_agents, num_tasks], 1.0 / num_agents), dtype=tf.float32)
 # kappa = tf.Variable([0., 1., 1., 0.], dtype=tf.float32)
 # mu is the allocation probability based on kappa
 mu = tf.nn.softmax(tf.reshape(kappa, shape=[num_agents, num_tasks]), axis=1)
-# mu = tf.constant([[0., 1.] ,[1., 0.]], dtype=tf.float32)
+# mu = tf.constant([[0., 1.] , [1., 0.]], dtype=tf.float32)
 # episodes reward is a deque FILO deque structure which
 episodes_reward = collections.deque(maxlen=min_episodes_criterion)
 # data writer, to store the data
 data_writer = AsyncWriter('data-cartpole-learning', 'data-cartpole-alloc', num_agents, num_tasks)
 print("mu ", mu)
-with tqdm.trange(10) as t:
+mu_thresh = np.ones([num_agents, num_tasks]) - np.ones([num_agents, num_tasks]) * 0.03
+with tqdm.trange(100000) as t:
     for i in t:
         initial_states = agent.get_initial_states()
         rewards_l, ini_values = agent.train_step(initial_states, max_steps_per_episode, mu, *models)
-        #if i % 5 == 0:
-        # agent.render_episode(max_steps_per_episode, *models)
-        # with tf.GradientTape() as tape:
-        #     mu = tf.nn.softmax(tf.reshape(kappa, shape=[num_agents, num_tasks]), axis=0)
-        #     print("mu ", mu)
-        #     alloc_loss = agent.compute_alloc_loss(ini_values, mu)  # alloc loss
-        # kappa_grads = tape.gradient(alloc_loss, kappa)
-        # processed_grads = [-0.001 * g for g in kappa_grads]
-        # kappa.assign_add(processed_grads)
+        if i % 200 == 0:
+            agent.render_episode(max_steps_per_episode, *models)
+        if i % 20 == 0:
+            with tf.GradientTape() as tape:
+                mu = tf.nn.softmax(tf.reshape(kappa, shape=[num_agents, num_tasks]), axis=0)
+                alloc_loss = agent.compute_alloc_loss(ini_values, mu)  # alloc loss
+            kappa_grads = tape.gradient(alloc_loss, kappa)
+            kappa.assign_add(alpha2 * kappa_grads)
         summed_rewards = tf.reduce_sum(rewards_l, 1)
-        print("summed rewards ", tf.reshape(summed_rewards, [-1]).numpy())
+        if i % 10 == 0:
+            print("v_ini \n", ini_values)
+            print("mu \n", mu)
         episode_reward = np.around(tf.reshape(summed_rewards, [-1]).numpy(), decimals=2)
         episodes_reward.append(episode_reward)
         running_reward = np.around(np.mean(episodes_reward, 0), decimals=2)
         data_writer.write({'learn': running_reward, 'alloc': mu.numpy()})
         t.set_description(f"Epsiode: {i}")
         t.set_postfix(running_reward=running_reward)
+        running_tasks = np.reshape(running_reward, [num_agents, num_tasks + 1])
+        running_tasks_ = running_tasks[:, 1:]
+        mu_term = (mu > mu_thresh).numpy().astype(np.float32)
+        allocated_task_rewards = mu_term * running_tasks_
+        task_term = all([np.any(np.greater_equal(allocated_task_rewards[:, i], e)) for i in range(num_tasks)])
+        if all(x > c for x in running_reward[::3]) and task_term:
+            break

a2c_team_tf/utils/plot_saved_data.py
@@ -6,15 +6,45 @@ import pandas as pd
 
 data = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..', 'data'))
 figures = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..', 'figures'))
-dfname = "experiment-cartpole-learning"
-title = "RL data"
+dfname = "exp1-cartpole-learning"
+dfname2 = "exp1-cartpole-alloc"
+title1 = "Learning Performance"
+title2 = "Agent 1 Task Allocation"
+title3 = "Agent 2 Task Allocation"
+title4 = "Task Performance"
+sfname = "exp1-cartpole-plots"
+title_size = 16
+tick_font_size = 12
+ival = 100  # sampling interval
+f_len = 8000
 df = pd.read_csv(f'{data}/{dfname}.csv', delimiter=',', header=None, names=["A1", "A1T1", "A1T2", "A2", "A2T1", "A2T2"])
-x = np.arange(0, df.shape[0], 1)
-plt.figure()
-ax = df.plot(y=["A1", "A2"])
-ax.set_xlabel('Episodes')
-ax.set_ylabel('Avg Reward')
-ax.set_title(f'{title}')
-ax.legend()
-plt.show()
-#plt.savefig(f'{figures}/{sfname}', dpi='figure', format='png', padinches=0)
+df2 = pd.read_csv(f'{data}/{dfname2}.csv', delimiter=',', header=None, names=["A1T1", "A1T2", "A2T1", "A2T2"])
+fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(15, 4))
+df.iloc[:f_len:ival, :].filter(items=['A1', 'A2']).plot(ax=axes[0])
+df2.iloc[:f_len:ival, :].filter(items=["A1T1", "A1T2"]).plot(ax=axes[1])
+df2.iloc[:f_len:ival, :].filter(items=["A2T1", "A2T2"]).plot(ax=axes[2])
+df.iloc[:f_len:ival, :].filter(items=['A1T2', 'A2T1']).plot(ax=axes[3])
+axes[0].set_xlabel('Episodes', size=title_size)
+axes[0].set_ylabel('Avg Reward', size=title_size)
+axes[0].set_title(f'{title1}', size=title_size)
+axes[0].legend(loc='lower right')
+axes[1].set_xlabel('Episodes', size=title_size)
+axes[1].set_ylabel('Allocation Probability', size=title_size)
+axes[1].set_title(f'{title2}', size=title_size)
+axes[1].legend(loc='lower right')
+axes[2].set_xlabel('Episodes', size=title_size)
+axes[2].set_ylabel('Allocation Probability', size=title_size)
+axes[2].set_title(f'{title3}', size=title_size)
+axes[2].legend(loc='lower right')
+axes[3].set_xlabel('Episodes', size=title_size)
+axes[3].set_ylabel('Success Rate (%)', size=title_size)
+axes[3].set_title(f'{title4}', size=title_size)
+axes[3].legend(loc='lower right')
+for i in range(axes.shape[0]):
+    for tick in axes[i].xaxis.get_major_ticks():
+        tick.label.set_fontsize(tick_font_size)
+    for tick in axes[i].yaxis.get_major_ticks():
+        tick.label.set_fontsize(tick_font_size)
+fig.tight_layout()
+#plt.show()
+plt.savefig(f'{figures}/{sfname}.png', dpi=300, format='png', padinches=0)

examples/cartpole_ex.py
@@ -25,14 +25,14 @@ class MoveToPos(DFAStates, ABC):
         self.fail = "F"
 
 def go_left_to_pos(data, _):
-    if data['state'][0] <= -0.5 and -45.0 < math.degrees(data['state'][2]) < 45.0:
+    if data['state'][0] <= -0.5 and -180.0 < math.degrees(data['state'][2]) < .0:
         return "P"
     elif not -45.0 < math.degrees(data['state'][2]) < 45.0:
         return "F"
     else:
         return "I"
 
-def go_right(data, _):
+def go_right_to_pos(data, _):
     if data['state'][0] > .6 and -45. < math.degrees(data['state'][2]) < 45.:
         return "P"
     elif not -45.0 < math.degrees(data['state'][2]) < 45.0:
@@ -49,7 +49,7 @@ def failed(data, _):
 def make_move_to_pos_dfa():
     dfa = DFA(start_state="I", acc=["P"], rej=["F"])
     dfa.states = MoveToPos()
-    dfa.add_state(dfa.states.init, go_right)
+    dfa.add_state(dfa.states.init, go_right_to_pos)
     dfa.add_state(dfa.states.position, finished_move)
     dfa.add_state(dfa.states.fail, failed)
     return dfa
@@ -109,7 +109,7 @@ running_reward = 0
 ## No discount
 gamma = 1.00
 alpha1 = 0.0001
-alpha2 = 0.01
+alpha2 = 0.0001
 learning_rate_theta = tf.keras.optimizers.schedules.ExponentialDecay(
     initial_learning_rate=alpha1,
     decay_steps=10000,
@@ -141,12 +141,14 @@ with tqdm.trange(max_episodes) as t:
         initial_states = agent.get_initial_states()
         rewards, ini_values = \
             agent.train_step(initial_states, max_steps_per_episode, mu, *models)
-        if i % 50 == 0 and i > 0:
+        if i % 5 == 0 and i > 0:
             with tf.GradientTape() as tape:
                 mu = tf.nn.softmax(tf.reshape(kappa, shape=[num_agents, num_tasks]), axis=0)
                 alloc_loss = agent.compute_alloc_loss(ini_values, mu)  # alloc loss
             kappa_grads = tape.gradient(alloc_loss, kappa)
+            print("kappa grads", kappa_grads)
             processed_grads = [-alpha2 * g for g in kappa_grads]
+            # print('processed grads', processed_grads)
             kappa.assign_add(processed_grads)
             mu = tf.nn.softmax(tf.reshape(kappa, shape=[num_agents, num_tasks]), axis=0)
         # Calculate the episode rewards
@@ -159,7 +161,7 @@ with tqdm.trange(max_episodes) as t:
             agent.render_episode(max_steps_per_episode, *models)
             print("mu \n", mu)
         t.set_description(f"Episode: {i}")
-        t.set_postfix(running_reward=running_reward)
+        t.set_postfix(running_reward=running_reward, alloc_loss=np.around(tf.reshape(mu, [-1]).numpy(), decimals=4))
 
         running_tasks = np.reshape(running_reward, [num_agents, num_tasks + 1])
         running_tasks_ = running_tasks[:, 1:]